b9bac0aac71039b968672269763b39c7b0243cc6
Christian Del Pino <delpic@users.noreply.github.com>
2019-09-26 14:43:23 -0400

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ connectionString=$(aws ecr get-login --region=us-east-1)
 cleanedConnection=$(echo $connectionString | sed -e "s/docker login -u //g" | sed -e "s/ -p//g" | sed -e "s/ -e none//g")
 connectionParts=($cleanedConnection)
 
-echo ::add-mask::${connectionParts[0]
+echo ::add-mask::${connectionParts[0]}
 echo ::set-output name=username::"${connectionParts[0]}"
 echo ::add-mask::${connectionParts[1]}
 echo ::set-output name=password::"${connectionParts[1]}"
